637510c40b08b4af65755b5f0d5c42dd8268415b
Joham Romucho <joham.romucho@pucp.edu.pe>
2023-06-13 21:44:01 -0500

    feat: add flavor dao methods

--- dao/nova/FlavorDao.py
+++ dao/nova/FlavorDao.py
@@ -1,7 +1,44 @@
+import requests
+import json
+
 class FlavorDao:
 
-    def __init__(self, base_url:str) -> None:
+    def __init__(self, base_url:str, token:str) -> None:
         self.url = base_url+"/v2.1/flavors"
+        self.token = token
 
-    def listFlavor():
-        pass
+
+    def listAllFlavor(self):
+        headers={"X-Auth-Token":self.token}
+        response = requests.get(url=self.url, headers=headers)
+        return response
+    
+    def createFlavor(self, flavor_name:str, ram:int, vcpus:int, disk:int):
+        headers={"X-Auth-Token":self.token}
+        body = {
+                "flavor": {
+                    "name": flavor_name,
+                    "ram": ram,
+                    "vcpus": vcpus,
+                    "disk": disk
+                }
+            }
+        response = requests.post(url=self.url, data=json.dumps(body), headers=headers)
+
+    def showFlavorDetail(self, flavor_id:str):
+        url = self.url +"/" + flavor_id
+        headers={"X-Auth-Token":self.token}
+        response=requests.get(url=url, headers=headers)
+        return response
+
+    def showFlavorsDetail(self):
+        url = self.url +"/detail"
+        headers={"X-Auth-Token":self.token}
+        response = requests.get(url=url, headers=headers)
+        return response
+    
+    def deleteFlavor(self, flavor_id:str):
+        url = self.url +"/" + flavor_id
+        headers={"X-Auth-Token":self.token}
+        response=requests.delete(url=url, headers=headers)
+        return response
